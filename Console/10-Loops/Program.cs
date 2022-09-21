de4df5b96d19cfd9bb344f6e8676b6daa72e98da
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-09-21 16:52:06 +0300

    C#
    C#

---
+++ Console/10-Loops/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace _10_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+
+            //faktoriyel hesabı
+
+            int sayi1;
+            Console.WriteLine("Faktoriyeli alınacak sayıyı girin: ");
+            sayi1 =Convert.ToInt16(Console.ReadLine());
+            int faktoryel = 1;
+
+            while(sayi1 > 1)
+            {
+                faktoryel = faktoryel * sayi1;
+                sayi1--;
+            }
+            Console.WriteLine(faktoryel);
+
+
+
+
+
+            //Tek sayı girilene kadar devam eden program
+            int sayi;
+            Console.WriteLine("Bir sayı girin: ");
+            sayi = Convert.ToInt16(Console.ReadLine());
+
+            while (sayi %2 == 0)
+            {
+                Console.WriteLine("Bir sayı girin");
+                sayi = Convert.ToInt16(Console.ReadLine());
+
+            }
+            Console.WriteLine("Tek sayı girdiniz");
+
+            int j;
+            j = 1;
+            while(j <= 500)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+
+
+            int i;
+            i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine("Merhaba Kahramanmaraş");
+                i++;
+            }
+
+
+
+
+            ForLoops3();
+            tekÇiftToplam();
+            sayılarınToplamı();
+            forLoops2();
+            forLoops1();
+            Console.ReadKey();
+
+        }
+
+        private static void ForLoops3()
+        {
+            //Fibonacci serisi
+            int a = 1;
+            int b = 1;
+            int c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int l = 1; l <= 8; l++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+            //9 ve 9'un katlarınını toplamı
+            int i;
+            for (i = 0; i <= 100; i++)
+            {
+                if (i % 9 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // 120 'nin tam bölenlerini yazdırma
+            int sayi = 120;
+            for (int j = 1; j <= 120; j++)
+            {
+                if (sayi % j == 0)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+        }
+
+        private static void sayılarınToplamı()
+        {
+
+            //Girilen sayıların toplamı uygulaması
+            int i;
+            int toplam = 0;
+            for (i = 1; i <= 10; i++)
+            {
+                toplam = toplam + i;
+
+            }
+            Console.WriteLine("Sayıların toplamı = " + toplam);
+        }
+
+        private static void tekÇiftToplam()
+        {
+            //çift sayıların toplamı
+            int n;
+            int toplam2 = 0;
+            for (n = 0; n <= 10; n += 2)
+            {
+                toplam2 = toplam2 + n;
+
+            }
+            Console.WriteLine("Sayıların toplamı = " + toplam2);
+
+            //Tek sayıların toplamı
+            int j;
+            int toplam1 = 0;
+            for (j = 1; j <= 10; j += 2)
+            {
+                toplam1 = toplam1 + j;
+
+            }
+            Console.WriteLine("Sayıların toplamı = " + toplam1);
+        }
+
+        private static void forLoops2()
+        {
+            int i;
+            for (i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            int j;
+            for (j = 0; j <= 20; j += 2) // j= j + 2;
+            {
+                Console.WriteLine(j);
+            }
+
+            int m;
+            for (m = 0; m <= 20; m += 3) // m= m + 3;
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+        private static void forLoops1()
+        {
+            //For looops bunu birkere yaz defalarca yazılsın
+
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+
+            //For Loops
+            int i;
+            for (i = 1; i <= 10; i++) // for(Başlangıç değeri; bu sayıyakad ; artır yada azalt)
+            {
+                Console.WriteLine("Merhaba Dünya");
+            }
+        }
+    }
+}
---
+++ Console/11 - Diziler/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace _11___Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diziler 2
+
+            string[] isim = new string[3];
+            int[] s1 = new int[3];
+            int[] s2 = new int[3];
+            int[] ort = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Clear(); //Öğrenci ad ve puandan sonra siler
+
+                Console.WriteLine(i + 1 + "Öğrencinin Adı: ");
+                isim[i] = Console.ReadLine();
+
+                Console.WriteLine(i+1+"Sınav: ");
+                s1[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(i+1+"Sınav2: ");
+                s2[i] = Convert.ToInt32(Console.ReadLine());
+
+                ort[i] = (s1[i] + s2[i]) / 2;
+
+            }
+            Console.WriteLine("Öğrenci  Sınav  Sınav2  Ortalama");
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("   " + isim[i]+"   " + s1[i] + "    " + s2[i]+"   " + ort[i]);
+            }
+            enBuyuk();
+            Dizi();
+            Console.ReadKey();
+        }
+
+        private static void enBuyuk()
+        {
+            int[] sayilar = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{(i + 1).ToString()}.Sayıyıyı girin: ");
+                sayilar[i] = Convert.ToInt16(Console.ReadLine());
+
+            }
+            int enbuyuk;
+            enbuyuk = sayilar[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (enbuyuk < sayilar[i])
+                {
+                    enbuyuk = sayilar[i];
+                }
+
+            }
+            Console.WriteLine(enbuyuk);
+        }
+
+        private static void Dizi()
+        {
+            //Diziler 
+            int[] sayilar = { 10, 45, 21, 35, 64, 75, 84, 26 };
+            Console.WriteLine(sayilar[0]);//dizinin ilk başta indeks değeri
+            Console.WriteLine(sayilar[7]); //dizinin en son indeks değeri 7
+
+            string[] isimler = { "Selim", "Ali", "Ahmet", "Emel", "Ayşe", "Baran" };
+            Console.WriteLine(isimler[3]);
+
+            string[] sehirler1 = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Şehir: ");
+                sehirler1[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("******Şehirler Listesi*****");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(sehirler1[i]);
+            }
+
+            string[] ogrenciler = new string[30];
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine("Öğrenci: ");
+                ogrenciler[i] = Console.ReadLine();
+                Console.WriteLine("Öğrenci ad soyad:" + ogrenciler[i]);
+            }
+        }
+    }
+}
